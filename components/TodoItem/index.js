cb2685aa4f6954b8e094fd769ce8b5df7e208ac2
lulucodingwizard <how.to.develop.fast@gmail.com>
2022-10-09 18:20:11 +1100

    managing state

--- components/TodoItem/index.js
+++ components/TodoItem/index.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../List/list.module.css";
 
 function TodoItem({ item }) {
+  const [isCompleted, setIsCompleted] = useState(item.completed);
+  const hundleCompletion = () => {
+    setIsCompleted(!isCompleted);
+  };
   return (
     <div key={item.key} className={styles.item}>
       <h4 className={styles.item_title}> {item.title}</h4>
-      <p> {item.completed ? "completed" : "not completed"}</p>
+      <p className={!isCompleted ? styles.notCompleted : ""}>
+        {" "}
+        {isCompleted ? "completed" : "not completed"}
+      </p>
+      <button onClick={hundleCompletion}>
+        {isCompleted ? "set to no completed" : "set completed"}
+      </button>
     </div>
   );
 }
